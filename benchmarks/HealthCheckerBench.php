3310f374a03313799953b614ee21168cd70e4980
v.razuvaev <rasuvaeff@gmail.com>
2026-06-21 17:15:07 +0300

    fix: sort use imports alphabetically in HealthCheckerBench

--- benchmarks/HealthCheckerBench.php
+++ benchmarks/HealthCheckerBench.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Rasuvaeff\Yii3HealthCheck\Benchmarks;
 
-use Rasuvaeff\Yii3HealthCheck\HealthResult;
 use Rasuvaeff\Yii3HealthCheck\HealthChecker;
+use Rasuvaeff\Yii3HealthCheck\HealthResult;
 use Testo\Bench;
 
 final class HealthCheckerBench
